d89feda6aeef601769f2848e7d919a9959a95eb9
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-03 16:32:15 +0100

    Initialize A1 env successfully

--- quadruped_bc/envs/__init__.py
+++ quadruped_bc/envs/__init__.py
@@ -10,3 +10,10 @@ register(
     max_episode_steps=1000,
     reward_threshold=6000.0,
 )
+
+register(
+    id="A1-v4",
+    entry_point="quadruped_bc.envs.a1:A1Env",
+    max_episode_steps=1000,
+    reward_threshold=6000.0,
+)
--- tests/envs/test_registration.py
+++ tests/envs/test_registration.py
@@ -3,3 +3,6 @@ import quadruped_bc.envs
 
 def test_my_ant_env():
     env = gym.make("MyAnt-v4")
+
+def test_quadruped_env():
+    env = gym.make("A1-v4")
